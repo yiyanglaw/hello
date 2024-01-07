0bd25fbd3a7daecbc7090f44aa43f15360ba6f63
lyxc <103878931+yiyanglaw@users.noreply.github.com>
2024-01-07 19:30:29 +0800

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,11 @@
+# Import the Streamlit library
+import streamlit as st
+
+# Define the main function to create the app
+def main():
+    # Use the st.write() function to display text
+    st.write("Hello")
+
+# Run the main function when the script is executed
+if __name__ == "__main__":
+    main()
